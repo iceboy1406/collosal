bd810de232f565cc560cda86025bdb230342d8ae
iceboy1406 <ekodoko880@gmail.com>
2022-06-08 08:29:03 +0700

    Change loading of navbrand too lazy ... again

--- components/atoms/NavBrand/index.tsx
+++ components/atoms/NavBrand/index.tsx
@@ -14,7 +14,7 @@ const NavBrand = () => {
       >
         <Link href={'/'} passHref>
           <div className="w-fit h-fit">
-            <Image src={'/images/brand.svg'} width='135px' height={30} alt='company brand' loading='eager' />
+            <Image src={'/images/brand.svg'} width='135px' height={30} alt='company brand' loading='lazy' />
           </div>
         </Link>
       </div>
